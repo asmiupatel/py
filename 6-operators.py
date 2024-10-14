9db222eff870b9687110b062332469593b9354a1
asmiupatel <liberteed1@gmail.com>
2024-10-14 15:11:03 -0400

    operators and operator precedence

---
+++ 6-operators.py
@@ -0,0 +1,26 @@
+a = 1.2
+print(a)
+
+b = 2 * 1.6
+print(b)
+
+c = 3
+d = 5.6
+e = c + d
+print(e)
+
+f = "Baby"
+g = "grill"
+h = f + g
+print(h)
+
+i = [1, 2, 4, 7, 9]
+j = [3, 5, 6, 8]
+k = i + j
+print(k)
+
+l = 4 * 14 + 10
+print(l)
+
+m = 4 * (14 + 10)
+print(m)
