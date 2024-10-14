f12e5901ec42cd4426528731455be2580f5672e1
asmiupatel <liberteed1@gmail.com>
2024-10-14 15:25:20 -0400

    relational operators

---
+++ 8-rela.py
@@ -0,0 +1,29 @@
+#relational operators (<, >, <=, >=, ==, !=)
+
+print(5 > 5)
+print(10 > 5)
+
+print("")
+
+print(5 < 5)
+print(5 < 10)
+
+print("")
+
+print(5 <= 5)
+print(10 <= 5)
+
+print("")
+
+print(5 >= 10)
+print(5 >= 5)
+
+print("")
+
+print(5 == 5)
+print(5 == 10)
+
+print("")
+
+print(10 != 5)
+print(5 != 5)
