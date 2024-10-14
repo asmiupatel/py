79d1dab13afa8625e50f64eb1858f23ea7e7a8cf
asmiupatel <liberteed1@gmail.com>
2024-10-14 14:54:23 -0400

    datatypes and conversion

---
+++ 5-datatype.py
@@ -0,0 +1,29 @@
+n = 3
+r = 34.56
+s = "gang"
+
+l = [10,20,"gun",40]
+
+print(n)
+print(type(n))
+print(r)
+print(type(r))
+print(s)
+print(type(s))
+print(l)
+print(type(l))
+print(l[2])
+print(type(l[2]))
+
+b = bool(0)
+b1 = bool(10)
+b2 = bool(-19)
+b3 = bool("0")
+b4 = bool("")
+b5 = bool(0.0)
+print(b)
+print(b1)
+print(b2)
+print(b3)
+print(b4)
+print(b5)
