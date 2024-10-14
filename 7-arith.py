b89e310c862a6dff7ff9935b902a42c71ac67c8a
asmiupatel <liberteed1@gmail.com>
2024-10-14 15:18:23 -0400

    arithmetic operators

---
+++ 7-arith.py
@@ -0,0 +1,8 @@
+print("addition", 7 + 4)
+print("subtraction", 7 - 4)
+print("multiplication", 7 * 4)
+print("division", 7 / 4)
+
+print("floor division", 7 // 4)
+print("modulus", 7 % 4)
+print("exponent", 2 ** 7)
