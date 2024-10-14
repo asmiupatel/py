3abf3104e0f33d52036282ce55846a4504b7ca46
asmiupatel <liberteed1@gmail.com>
2024-10-14 15:39:14 -0400

    string basic

---
+++ 10-str.py
@@ -0,0 +1,18 @@
+a = "baby"
+b = "grill"
+print(a)
+print(b)
+print(a + b)
+
+print("")
+
+c = "no"
+d = "bandana"
+print(d[0:2], c[0], d[4:7])
+
+print("")
+
+e = '23456789'
+f = int(e[1])
+g = int(e[4])
+print(f+g)
