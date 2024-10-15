53174e02455d259aa2071a23d59746e9a4b61ab3
asmiupatel <liberteed1@gmail.com>
2024-10-14 22:07:28 -0400

    string func

---
+++ 11-strtwo.py
@@ -0,0 +1,33 @@
+s = 'gang'
+print(s * 4)
+print("")
+t = 'Hope'
+print(t == 'Hope')
+print(t == 'hope')
+print("")
+print('city' < 'town')
+print('switch' < 'plug')
+print('abcdef' < 'abcde')
+print("")
+u = 'chamber'
+print(u[0])
+print(u[1])
+print(u[2])
+print(u[3])
+print(u[4])
+print(u[5])
+print(u[6])
+print("")
+print("reverse")
+print(u[-1])
+print(u[-2])
+print(u[-3])
+print(u[-4])
+print(u[-5])
+print(u[-6])
+print(u[-7])
+print("")
+v = 'fvfdbjfddfbrhejdgisidfoqeifsdcoeiebvskvboef efifoifowiefef'
+print(len(v))
+print(v[len(v)-1]) #prints the last character of the string after calculating the lengyh of the string
+
