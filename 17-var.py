28c79dc463f834c281af042c7aaae94935ecce24
asmiupatel <liberteed1@gmail.com>
2024-10-17 18:05:27 -0400

    variables and comments

---
+++ 17-var.py
@@ -0,0 +1,13 @@
+rams_bank_balance = 100000
+rams_loan = 200000
+
+lakshmans_bank_balance = 1000000
+lakshmans_loan = 200000
+
+net_income = rams_bank_balance + lakshmans_bank_balance #net income is the total of bank balances of both ram and lakshman
+
+net_liability = rams_loan + lakshmans_loan #net liability is teh total of loans taken by both ram and lakshman
+
+final_value = net_income - net_liability #shows how much money they have left after paing off the loan, could be negative too
+
+print(final_value)
