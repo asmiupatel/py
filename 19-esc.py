ad11770b16cd4cd7728b98d0b378e9ac9c377664
asmiupatel <liberteed1@gmail.com>
2024-10-17 20:19:50 -0400

    escape chars and extra

---
+++ 19-esc.py
@@ -0,0 +1,22 @@
+print('It\'s a beautiful day')
+print("We go \"ZooooooooM\"")
+print("I am a human.\nI am O-negative.\n")
+print("I am a human.\tI am O-negative.")
+
+x = "This is a line."
+y = 'This is a line.'
+z = '''One
+Two
+Three
+Four
+Five
+Cho
+'''
+
+'''This is a comment
+Used for a variable, triple quotes become a multiline string
+when used alone, it becomes a multiline comment'''
+
+print(x)
+print(y)
+print(z)
