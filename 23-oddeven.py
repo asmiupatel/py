d11146f0cd4e129ed00caed699ce68ceedb28579
asmiupatel <liberteed1@gmail.com>
2024-10-26 21:16:49 -0400

    odd even

---
+++ 23-oddeven.py
@@ -0,0 +1,9 @@
+print("Please enter the number you want to check is even or odd:")
+
+index = int(input())
+
+if (index % 2 == 0) :
+    print(index , "is an even number.")
+else :
+    print(index , "is an odd number.")
+      
