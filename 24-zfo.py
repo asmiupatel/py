334e1e77ac85b093cebf9234bd9df923fe6c3e25
asmiupatel <liberteed1@gmail.com>
2024-10-26 21:33:27 -0400

    0, 5 or other

---
+++ 24-zfo.py
@@ -0,0 +1,10 @@
+print("Please enter the number you want to check ends with 0 or 5 or other:")
+
+index = int(input())
+
+if (index % 10 == 0) :
+    print(index ,"ends with 0.")
+elif (index % 10 == 5)    :
+    print(index ,"ends with 5.")
+else :
+    print(index ,"ends with something other than 0 or 5.")   
