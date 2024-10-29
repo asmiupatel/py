3027fc90af5959e151003873744bd55953822aa7
asmiupatel <liberteed1@gmail.com>
2024-10-28 21:51:22 -0400

    calender

---
+++ 29-calendar.py
@@ -0,0 +1,15 @@
+# import calendar
+# print(calendar.month(2001,9))
+# print(calendar.calendar(2001))
+
+# from calendar import month,calendar
+# print(calendar(2002))
+# print(month(2002,9))
+
+# import calendar as c
+# print(c.month(2002,9))
+
+from calendar import month as m
+print(m(2001,9))
+
+#i spelled it calender
