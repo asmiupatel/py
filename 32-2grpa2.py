9c52b726cf3f8a1a24a1994c036262d40240db7f
asmiupatel <liberteed1@gmail.com>
2024-10-28 23:06:06 -0400

    w2 grpa2

---
+++ 32-2grpa2.py
@@ -0,0 +1,16 @@
+# A single quote ' and a double quote "
+output1 = "This is a single quote ' and this is a double quote \""
+
+# A forward slash / and a backward slash \
+output2 = "This is a forward slash / and this is a backward slash \\"
+
+# Three single quotes ''' and three double quotes """
+output3 = "These are three single quotes ''' and these are three double quotes \"\"\""
+
+# Double forward slash // and Double backward slash \\
+output4 = "These are double forward slashes // and these are double ackward slashes \\\\"
+
+print(output1)
+print(output2)
+print(output3)
+print(output4)
